73a467a99fb9f38424c4b3531457b31b56f5948e
GulchexrasGithub <burgulchexra@gmail.com>
2023-06-17 19:19:06 +0500

    BROKERS: Add Logging Broker

---
+++ UserAPI/Brokers/Loggings/ILoggingBroker.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace UserAPI.Brokers.Loggings
+{
+    public interface ILoggingBroker
+    {
+        void LogError(Exception exception);
+        void LogCritical(Exception exception);
+    }
+}
