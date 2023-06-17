544291f6f14869663371a21ca28f155b9903ec19
GulchexrasGithub <burgulchexra@gmail.com>
2023-06-17 19:21:32 +0500

    BROKERS: User Broker CRUD

--- UserAPI/Brokers/Storages/IStorageBroker.User.cs
+++ UserAPI/Brokers/Storages/IStorageBroker.User.cs
@@ -1,5 +1,14 @@
-﻿namespace UserAPI.Brokers.Storages
+﻿using System.Linq;
+using System.Threading.Tasks;
+using UserAPI.Models.Users;
+
+namespace UserAPI.Brokers.Storages
 {
     public partial interface IStorageBroker
-    { }
+    {
+        ValueTask<User> InsertUserAsync(User user);
+        IQueryable<User> SelectAllUsers();
+        ValueTask<User> UpdateUserAsync(User user);
+        ValueTask<User> DeleteUserAsync(User user);
+    }
 }
--- UserAPI/Brokers/Storages/StorageBroker.User.cs
+++ UserAPI/Brokers/Storages/StorageBroker.User.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using UserAPI.Models.Users;
 
 namespace UserAPI.Brokers.Storages
@@ -6,5 +8,17 @@ namespace UserAPI.Brokers.Storages
     public partial class StorageBroker
     {
         public DbSet<User> Users { get; set; }
+
+        public async ValueTask<User> InsertUserAsync(User user) =>
+         await InsertAsync(user);
+
+        public IQueryable<User> SelectAllUsers() =>
+            SelectAll<User>();
+
+        public async ValueTask<User> UpdateUserAsync(User user) =>
+            await UpdateAsync(user);
+
+        public async ValueTask<User> DeleteUserAsync(User user) =>
+            await DeleteAsync(user);
     }
 }
